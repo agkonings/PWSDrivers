a0cc7d63d51c708c7c3672ca3341d263908f56ba
agkonings <konings@stanford.edu>
2023-05-05 15:43:13 -0700

    double check feature correlations with PWS are cosnistent with RF importance

--- pickFeatures.py
+++ pickFeatures.py
@@ -10,8 +10,14 @@ import os
 import numpy as np
 import pandas as pd
 from osgeo import gdal
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr, spearmanr
 
 import dirs
+
+#sns.set(font_scale = 1.2, style = "ticks")
+#plt.rcParams.update({'font.size': 20})
 
 #load features 
 def add_pws(df, pwsPath):
@@ -51,7 +57,7 @@ def load_data(dfPath, pwsPath):
     return df
 
 dfPath = os.path.join('C:/repos/data/inputFeatures_wgNATSGO_wBA.h5')
-pwsPath = 'G:/My Drive/0000WorkComputer/dataStanford/PWS_through2021_JunThruNov.tif'
+pwsPath = 'G:/My Drive/0000WorkComputer/dataStanford/PWS_through2021.tif'
 df =  load_data(dfPath, pwsPath)
 
 #find data with valid PWS
@@ -92,7 +98,60 @@ and where have data that overlaps with species
 #calculate with all dominant species
 df_wSpec =  load_data(dfPath, pwsPath)
 df_wSpec.drop(['elevation','isohydricity','root_depth','p50','gpmax','c','g1','species','lat','lon'], axis = 1, inplace=True)
+df_wSpec.dropna(inplace = True)
 
 corrMatSpec = df_wSpec.corr()
 #manually investigated as long as R<0.75
-specDropList = ['dry_season_length','t_mean','AI','ppt_lte_100','elevation','species','lat','lon']
+specDropList = ['dry_season_length','t_mean','AI','ppt_lte_100']
+df_wSpec.drop(specDropList, axis = 1, inplace=True)
+df_wSpec.dropna(inplace = True)
+
+
+
+def plot_all_2Ddensities(df):
+    columns = df.columns
+    
+    # Determine the number of rows and columns for subplots
+    #assume 5 columns per row
+    num_columns = 4
+    num_rows = int( np.ceil(len(columns)/num_columns) )
+    
+
+    # Create the figure and subplots
+    fig, axes = plt.subplots(num_rows, num_columns, figsize=(20, 15))
+
+    #calculate correlations to sort figures
+    abs_pears = {}
+    for i, column in enumerate(columns):
+        if column != 'pws':             
+            pearsonVal, _ = pearsonr(df['pws'], df[column])
+            abs_pears[column] = np.abs( pearsonVal )
+    
+    #now re-arrange columns to go from highest to lowest Pearson
+    corr_df = pd.DataFrame.from_dict(abs_pears, orient='index', columns=['Pearson'])
+    sorted_columns = corr_df['Pearson'].sort_values(ascending=False).index
+
+    # Iterate over each column and create a heatmap subplot    
+    for i, column in enumerate(sorted_columns):
+        
+        if column != 'pws': 
+            #re-calculate correlations
+            pearson_corr, _ = pearsonr(df['pws'], df[column])    
+            spearman_corr, _ = spearmanr(df['pws'], df[column])
+            
+            #plotting
+            ax = axes[(i) // num_columns, (i) % num_columns]
+            sns.kdeplot(data=df, x='pws', y=column, ax=ax, cmap='YlOrRd', fill=True)
+            ax.set_xlim(0, 1.6)
+            if column == 'ks':
+                ax.set_ylim(0, 150)
+            text = f"$R_{{Pears}}$: {pearson_corr:.2f}\n$R_{{Spear}}$: {spearman_corr:.2f}"
+            ax.text(0.05, 0.95, text, transform=ax.transAxes, fontsize=10, verticalalignment='top')
+
+        
+    fig.tight_layout()
+
+    # Display the figure
+    plt.show()
+
+plot_all_2Ddensities(df_wSpec)
